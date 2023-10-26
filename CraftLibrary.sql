a6e1a2fd9f6945c9393053a8bfb9bd351044652c
morsc0 <138694853+morsc0@users.noreply.github.com>
2023-10-26 11:44:25 +0100

    Update CraftLibrary.sql
    added licensing info

--- CraftLibrary.sql
+++ CraftLibrary.sql
@@ -402,6 +402,25 @@ HAVING Craft_Types.CraftName = "Crochet";
 # Add trigger to update project status when project end date entered
 ####################################################################################
 
+
+####################################################################################
+## Licensing
+####################################################################################
+	Craft Library Copyright (C) 2023 Morag Gladstone
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+	
 ####################################################################################
 ## end of file
 ####################################################################################
